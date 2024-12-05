9be9d5f34847f5e0862a11cc404d58bf9e1a4331
Akalanka Dilshan <61039787+AkalankaDilshan@users.noreply.github.com>
2024-12-05 13:13:24 +0530

    feat: call vpc module

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
 provider "aws" {
   region = var.region
 }
+
+module "markerting_vpc" {
+  source               = "./modules/vpc"
+  vpc_name             = "Marketing-VPC"
+  cidr_block           = "10.0.0.0/16"
+  availability_zones   = ["eu-north-1a", "eu-north-1b", "eu-north-1c"]
+  public_subnet_cidrs  = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  private_subnet_cidrs = ["10.10.4.0/24", "10.10.5.0/24", "10.10.6.0/24"]
+}
--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -7,18 +7,20 @@ resource "aws_vpc" "main_vpc" {
 }
 
 resource "aws_subnet" "public_subnet" {
-  count      = length(var.public_subnet_cidrs)
-  vpc_id     = aws_vpc.main_vpc.id
-  cidr_block = element(var.public_subnet_cidrs, count.index)
+  count             = length(var.public_subnet_cidrs)
+  vpc_id            = aws_vpc.main_vpc.id
+  cidr_block        = element(var.public_subnet_cidrs, count.index)
+  availability_zone = element(var.availability_zones, count.index)
   tags = {
     Name = "public subnet ${count.index + 1}"
   }
 }
 
 resource "aws_subnet" "private_subnet" {
-  count      = length(var.private_subnet_cidrs)
-  vpc_id     = aws_vpc.main_vpc.id
-  cidr_block = element(var.private_subnet_cidrs, count.index)
+  count             = length(var.private_subnet_cidrs)
+  vpc_id            = aws_vpc.main_vpc.id
+  cidr_block        = element(var.private_subnet_cidrs, count.index)
+  availability_zone = element(var.availability_zones, count.index)
   tags = {
     Name = "private subnet ${count.index + 1}"
   }
