a7a19512c155a126469932d9c6de34857843d6a2
Akalanka Dilshan <61039787+AkalankaDilshan@users.noreply.github.com>
2024-12-10 23:39:12 +0530

    feat: create network ACL

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -86,12 +86,12 @@ resource "aws_vpc_peering_connection" "peering_connection" {
   }
 }
 
-# resource "aws_network_acl" "public_ACL" {
-#   vpc_id = aws_vpc.main_vpc.id
-#   tags = {
-#     Name = "${var.vpc_name}-public-NACL"
-#   }
-# }
+resource "aws_network_acl" "public_ACL" {
+  vpc_id = aws_vpc.main_vpc.id
+  tags = {
+    Name = "${var.vpc_name}-public-NACL"
+  }
+}
 
 # resource "aws_network_acl" "private_ACL" {
 #   vpc_id = aws_vpc.main_vpc.id
@@ -99,3 +99,51 @@ resource "aws_vpc_peering_connection" "peering_connection" {
 #     Name = "${var.vpc_name}-private-NACL"
 #   }
 # }
+
+resource "aws_network_acl_rule" "public_ssh_inbound" {
+  network_acl_id = aws_network_acl.public_ACL.id
+  rule_number    = 100
+  rule_action    = "allow"
+  protocol       = "6"
+  egress         = false
+  cidr_block     = "0.0.0.0/0"
+  from_port      = 22
+  to_port        = 22
+}
+
+resource "aws_network_acl_rule" "public_ssh_outbound" {
+  network_acl_id = aws_network_acl.public_ACL.id
+  rule_number    = 100
+  rule_action    = "allow"
+  protocol       = "6"
+  egress         = true
+  cidr_block     = "0.0.0.0/0"
+  from_port      = 22
+  to_port        = 22
+}
+
+# Allow all outbound traffic for public subnets
+resource "aws_network_acl_rule" "public_all_outbound" {
+  network_acl_id = aws_network_acl.public.id
+  rule_number    = 200
+  protocol       = "-1" # All protocols
+  rule_action    = "allow"
+  egress         = true
+  cidr_block     = "0.0.0.0/0"
+}
+
+# Deny all other inbound traffic for public subnets
+resource "aws_network_acl_rule" "public_deny_all_inbound" {
+  network_acl_id = aws_network_acl.public.id
+  rule_number    = 300
+  protocol       = "-1" # All protocols
+  rule_action    = "deny"
+  egress         = false
+  cidr_block     = "0.0.0.0/0"
+}
+
+resource "aws_subnet_network_acl_association" "subnet_network_acl" {
+  for_each       = aws_subnet.public_subnet
+  subnet_id      = each.value.id
+  network_acl_id = aws_network_acl.public_ACL.id
+}
