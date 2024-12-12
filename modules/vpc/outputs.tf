0a7cf4992e6928ec87392735d9627b09eb458349
Akalanka Dilshan <61039787+AkalankaDilshan@users.noreply.github.com>
2024-12-12 19:01:11 +0530

    fix : peering connection output issue

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -1,7 +1,6 @@
 output "vpc_id" {
   value = aws_vpc.main_vpc.id
 }
-
 output "vpc_name" {
   value = var.vpc_name
 }
@@ -12,11 +11,7 @@ output "public_subnet_id" {
   value = aws_subnet.public_subnet[*].id
 
 }
-
 output "private_subnet_id" {
   value = aws_subnet.private_subnet[*].id
 }
 
-output "peering_connection_id" {
-  value = aws_vpc_peering_connection.peering_connection.id
-}
