ef9d4059dc7d3fdcdc0fa12a51b7029f774c5ed5
Akalanka Dilshan <61039787+AkalankaDilshan@users.noreply.github.com>
2024-12-12 09:11:40 +0530

    fix : ami issue

--- modules/EC2/main.tf
+++ modules/EC2/main.tf
@@ -15,7 +15,7 @@ data "aws_ami" "latest_ami" {
 }
 
 resource "aws_instance" "server_instance" {
-  ami                         = data.aws_ami.ubuntu.id
+  ami                         = data.aws_ami.latest_ami.id
   instance_type               = var.instance_type
   subnet_id                   = var.subnet_id
   vpc_security_group_ids      = [var.security_group]
